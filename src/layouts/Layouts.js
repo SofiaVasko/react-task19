6858f4cc44251ad2ff0690ad6152a9719b9b0897
SofiaVasko <127936404+SofiaVasko@users.noreply.github.com>
2025-04-14 00:03:14 +0300

    Update Layouts.js

--- src/layouts/Layouts.js
+++ src/layouts/Layouts.js
@@ -4,10 +4,10 @@ const Layout = () => {
   return (
     <div>
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/post">Post</Link>
-        <Link to="/photos">Photos</Link>
-        <Link to="/contacts">Contacts</Link>
+        <Link to="/react-task19/">Home</Link>
+        <Link to="/react-task19/post">Post</Link>
+        <Link to="/react-task19/photos">Photos</Link>
+        <Link to="/react-task19/contacts">Contacts</Link>
       </nav>
       <Outlet />
     </div>
